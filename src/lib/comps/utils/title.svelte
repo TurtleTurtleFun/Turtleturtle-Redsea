99ddbc032f965a9ca0eff58ff017543d0214bdbf
fred wordie <fred@mildlyupset.com>
2025-12-22 12:40:00 +0100

    ignore file and title

--- src/lib/comps/utils/title.svelte
+++ src/lib/comps/utils/title.svelte
@@ -7,18 +7,39 @@
     })
 </script>
 
-<h1 class:notHome={!home}>TurtleTurtle</h1>
+<div class="title" class:notHome={!home}>
+    <h1>TurtleTurtle</h1>
+    <h2>Red Sea</h2>
+</div>
 
 <style lang="scss">
-    h1 {
-        font-family: 'Intro Script';
-        font-size: calc(1.2 * var(--font-size-xxxl));
-        -webkit-text-stroke: calc(0.5 * var(--padding)) var(--white);
-        color: var(--green);
-        paint-order: stroke fill;
-        text-shadow: var(--shadowS);
+    .title {
         transition: flex-grow var(--animationTime) ease;
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        justify-content: center;
+        h2 {
+            font-family: 'Intro Script';
+            font-size: calc(0.6 * var(--font-size-xxxl));
+            -webkit-text-stroke: calc(0.5 * var(--padding)) var(--red);
+            color: var(--white);
+            paint-order: stroke fill;
+            text-shadow: var(--shadowS);
+            align-self: flex-end;
+            rotate: -10deg;
+            margin-top: calc(-0.7 * var(--font-size-xxxl));
+            margin-right: calc(-1 * var(--padding));
+        }
 
-
+        h1 {
+            font-family: 'Intro Script';
+            font-size: calc(1.2 * var(--font-size-xxxl));
+            -webkit-text-stroke: calc(0.5 * var(--padding)) var(--white);
+            color: var(--green);
+            paint-order: stroke fill;
+            text-shadow: var(--shadowS);
+            margin-bottom: 0px;
+        }
     }
 </style>
